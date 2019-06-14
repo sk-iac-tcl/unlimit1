79ddf83ddc59d76984f146d27ee8653e0a4a8aea
unlimit1 <unlimit@gmail.com>
2019-06-14 16:39:49 +0900

    tags name 추가

--- chap2/main.tf
+++ chap2/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-40d28157"
-    instance_type = "t2.micro"
+  ami           = "ami-40d28157"
+  instance_type = "t2.micro"
 
-    tags {
-        Name = "terraform-example-by lyh"
-    }
+  tags = {
+    Name = "terraform-example by lyh"
+  }
 }
